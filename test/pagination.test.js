a76bd234a21725324f4121fd0ed58db70c2dec99
ahmedovv123 <ahmedovv123@gmail.com>
2023-01-25 11:56:42 +0200

    feat: Install jest

---
+++ test/pagination.test.js
@@ -0,0 +1,9 @@
+// const useGlobalComposable = require("../src/composables/globalComposable.js");
+// const listTools1 = useGlobalComposable(1, 5, true);
+// const demoData = require("../demo-data/demoData1.json");
+// const { test } = require("vitest");
+// listTools1.init(demoData);
+
+// test('Proper init of pagination', () => {
+//     console.log(listTools1.paginationUtils.paginate());
+// })
