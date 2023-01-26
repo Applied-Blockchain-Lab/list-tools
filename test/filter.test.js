f6424fc0750ceee56ceebcec40715bcbd194885e
ahmedovv123 <ahmedovv123@gmail.com>
2023-01-26 11:51:39 +0200

    test: Add test

--- test/filter.test.js
+++ test/filter.test.js
@@ -3,6 +3,7 @@ import useGlobalComposable from "../src/composables/globalComposable";
 import { readFileSync } from "fs";
 import { setActivePinia, createPinia } from "pinia";
 import { describe, /* expect, test, */ beforeEach } from "@jest/globals";
+import { it } from "node:test";
 const demoData = JSON.parse(readFileSync("./demo-data/demoData1.json"));
 
 describe("Filter tests", () => {
@@ -16,4 +17,6 @@ describe("Filter tests", () => {
     listTools1 = useGlobalComposable(STORE_ID, ITEMS_PER_PAGE, SINGLE_SORT);
     listTools1.init(demoData);
   });
+
+  it("Filters correctly by number", () => {});
 });
